54b3ac284da225d95c429cd568fe50a36a7688b5
hjdl001 <hjdl001@bucknell.edu>
2026-09-22 15:28:22 -0400

    project 3

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,42 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+//// approach: use the keyboard memory-mapped register to check if a key is pressed. 
+////if a key is pressed, write 0xFFFF to every pixel in the screen memory. 
+//// ff no key is pressed, write 0x0000 to every pixel in the screen memory.
+
+(LOOP)
+    // check if a key is currently being pressed
+    @KBD
+    D=M
+
+    // if the keyboard value is zero, no key is being pressed
+    @WHITE
+    D;JEQ
+
+(BLACK)
+    // set the color value to black
+    @color
+    M=-1
+
+    // start filling at the first screen address
+    @SCREEN
+    D=A
+    @address
+    M=D
+
+(FILL_BLACK)
+    // fill the screen with black
+    @address
+    A=M
+    M=D
+    @address
+    M=M+1
+    @FILL_BLACK
+    D;JLT
+
+(WHITE)
+    // if the keyboard value is not zero, a key is being pressed
+    // set the color value to white
+    @color
+    M=0     
